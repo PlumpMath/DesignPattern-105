5baeb6aadff7486a3c9a837a81f07fbbecd2c6a4
yangxiaoguo <yang08013271@gmail.com>
2017-01-18 15:28:44 +0800

    策略模式添加完成

---
+++ src/strategy_pattern/Duck.java
@@ -0,0 +1,35 @@
+package strategy_pattern;
+
+public abstract class Duck {
+	
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	
+	public Duck(){
+		
+	}
+
+	public abstract void display();
+	
+	public void performFly(){
+		flyBehavior.fly();
+	}
+	
+	public void performQuauk(){
+		quackBehavior.quack();
+	}
+	
+	public void swim(){
+		System.out.println("All ducks float,even decoys");
+	}
+
+	public void setFlyBehavior(FlyBehavior fb) {
+		flyBehavior = fb;
+	}
+
+	public void setQuackBehavior(QuackBehavior qb) {
+		quackBehavior = qb;
+	}
+
+	
+}
